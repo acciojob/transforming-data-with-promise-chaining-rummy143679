69ec5ee7fb07cb3ebab75c8b51295784a0cc5bbd
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-06-19 19:39:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 const start = document.getElementById("btn");
-const input = document.getElementById("number");
+const input = document.getElementById("ip");
 const output = document.getElementById("output");
-start.addEventListener("click", async () => {
-	await simple(input.value).then(res => {
+start.addEventListener("click", () => {
+	simple(input.value).then(res => {
 		output.innerHTML = `Result: ${res}`;
 		return multiplay(res);
 	}).then(res => {
@@ -17,39 +17,39 @@ start.addEventListener("click", async () => {
 		return fifth(res);
 	}).then(res => {
 		output.innerHTML = `Result: ${res}`;
-	})
+	}).catch(e => console.log(e));
 })
 
-async function simple(val){
-		return await new Promise((resolve, reject) => {
+function simple(val){
+		return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				resolve(val);
 			}, 2000);
 		})
 	}
-async function multiplay(val){
-		return await new Promise((resolve, reject) => {
+function multiplay(val){
+		return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				resolve(val * 2);
 			}, 2000);
 		})
 	}
-async function subtract(val){
-		return await new Promise((resolve, reject) => {
+function subtract(val){
+		return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				resolve(val - 3);
 			}, 1000);
 		})
 	}
-async function divied(val){
-		return await new Promise((resolve, reject) => {
+function divied(val){
+		return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				resolve(val / 2);
 			}, 1000);
 		})
 	}
-async function fifth(val){
-		return await new Promise((resolve, reject) => {
+function fifth(val){
+		return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				resolve(val);
 			}, 1000);
